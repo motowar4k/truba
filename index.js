ff28f45b834f5fbd7c9ddd8ddde6e11a101a805a
Matthew Trubin <80651949+motowar4k@users.noreply.github.com>
2021-10-27 23:34:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ class Calculator {
 
     appendNumber(number) {// добавление числа на экран
 
-        if (this.currentOperand.length < 7) {
+        /*if (this.currentOperand.length < 7) {
             if (number === '.' && this.currentOperand.includes('.')) return
 
             this.currentOperand = this.currentOperand.toString() + number.toString()
-        }
+        }*/
     }
 
 
